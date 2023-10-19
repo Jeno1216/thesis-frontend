0a9c29405c66b7ad9672b622dc02b2f49724ff9d
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2023-10-19 22:47:50 +0800

    ssss

--- src/Home.jsx
+++ src/Home.jsx
@@ -45,7 +45,7 @@ function Home() {
 
   useEffect(() => {
     axios
-      .get('http://localhost:8000/')
+      .get('https://thesis-backend-41ta.onrender.com/')
       .then((response) => {
         setMessage(response.data.message);
       })
@@ -117,7 +117,7 @@ function Home() {
     const endLng = endLatLng.lng;
 
     try {
-      const response = await axios.post('http://localhost:8000/find_path', {
+      const response = await axios.post('https://thesis-backend-41ta.onrender.com/find_path', {
         start_lat: startLat,
         start_lon: startLng,
         end_lat: endLat,
